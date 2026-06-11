0c7d5f0f9eaec776de39438bdfa77ce5bd76bb41
Lalesh V. Kokat <119997530+lalesh-dev@users.noreply.github.com>
2026-06-12 02:45:27 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+# from controllers.clientregister_controller import client_bp 
+from configs.mysql_config import MYSQLConfig
+from extension.sqlalchemy import db
+from controllers.user_controller import user_bp
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+app.config.from_object(MYSQLConfig)
+db.init_app(app)
+
+# app.register_blueprint(client_bp)
+app.register_blueprint(user_bp)
+
+with app.app_context():
+    db.create_all()
+
+print("/n Registered Endpoint :/n")
+for rule in app.url_map.iter_rules():
+    method = ',' .join (rule.methods  - { 'HEAD','OPTIONS'})
+
+    print(f"Endpoint : {rule.endpoint}")
+    print(f"Methods : {rule.methods}")
+    print(f"URL : {rule.rule}")
+    print("-" * 50 )
+
+if __name__=='__main__':
+    app.run(debug = True)    
